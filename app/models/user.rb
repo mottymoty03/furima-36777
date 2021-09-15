ea719ae34ae140f58b323331f5af3d1add3d914a
mottymoty03 <k.skmt9406@gmail.com>
2021-09-15 14:55:05 +0900

    新規登録時の入力のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[0-9])[a-z\d]{6,}\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
-  validates :lastname_kana, presence: true
-  validates :firstname_kana, presence: true
+  validates :lastname, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :firstname, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :lastname_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :firstname_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
   validates :birthday, presence: true
 end
