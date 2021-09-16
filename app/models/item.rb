2135a082901bbe62a437e5142573280fa4ad4af8
mottymoty03 <k.skmt9406@gmail.com>
2021-09-16 17:12:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,14 @@ class Item < ApplicationRecord
   belongs_to :burden
   belongs_to :area_info
   belongs_to :days_to_send
+
+  validates :itemname, presence: true
+  validates :itemdescription, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :itemstatus_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :burden_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :area_info_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :days_to_send_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
