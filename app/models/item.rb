1bf2e165b5f0ccbf7b3c334676689686e9c9a0f1
mottymoty03 <k.skmt9406@gmail.com>
2021-09-17 17:38:49 +0900

    指摘修正：画像及び価格の範囲のバリデーションが抜けていたのを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,10 @@ class Item < ApplicationRecord
   belongs_to :area_info
   belongs_to :days_to_send
 
+  validates :image, presence: true
   validates :itemname, presence: true
   validates :itemdescription, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :itemstatus_id, numericality: { other_than: 1, message: "can't be blank" } 
