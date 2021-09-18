b951f2c89a64801c1c905801c35e2d59638d0c43
mottymoty03 <k.skmt9406@gmail.com>
2021-09-18 20:10:43 +0900

    ルーティング、コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :itemname, :itemdescription, :category_id, :itemstatus_id, :burden_id, :area_info_id, :days_to_send_id, :price).merge(user_id: current_user.id)
