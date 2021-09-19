fb7141e4c5f0cf9a2d3a2c16d57604f1235e436b
mottymoty03 <k.skmt9406@gmail.com>
2021-09-19 17:15:54 +0900

    バリデーションの設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,8 @@
 class ShippingAddress < ApplicationRecord
+  validates :postal_code, presence: true
+  validates :municipality, presence: true
+  validates :house_number, presence: true
+  validates :phone_number, presence: true
+
+  validates :area_info_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
