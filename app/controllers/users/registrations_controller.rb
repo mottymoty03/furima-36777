49c3754b55ff4aca474e0acbed7c6f7e92d275a6
mottymoty03 <k.skmt9406@gmail.com>
2021-10-10 03:16:27 +0900

    コントローラーの設定

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  def create
+    if params[:sns_auth] == 'true'
+      pass = Devise.friendly_token
+      params[:user][:password] = pass
+      params[:user][:password_confirmation] = pass
+    end
+    super
+  end
+end
