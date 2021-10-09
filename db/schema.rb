92cbeb1bb1db2ce230d04fb0573cb2d15347d6f4
mottymoty03 <k.skmt9406@gmail.com>
2021-10-10 03:14:33 +0900

    モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_080649) do
+ActiveRecord::Schema.define(version: 2021_10_09_171056) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 2021_09_19_080649) do
     t.index ["purchase_id"], name: "index_shipping_addresses_on_purchase_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,4 +102,5 @@ ActiveRecord::Schema.define(version: 2021_09_19_080649) do
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
   add_foreign_key "shipping_addresses", "purchases"
+  add_foreign_key "sns_credentials", "users"
 end
